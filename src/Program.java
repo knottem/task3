46d4087bb6e7c20efc06a304d5394062788d3eeb
knottem <59744487+knottem@users.noreply.github.com>
2022-10-27 17:44:50 +0200

    testade lite

--- src/Program.java
+++ src/Program.java
@@ -8,6 +8,10 @@ public class Program {
     JPanel panel = new JPanel();
     ArrayList<JButton> buttons = new ArrayList<>();
     int tileSize = 100;
+    JButton tommaRutan = new JButton();
+
+    int width = 0;
+    int height = 0;
 
     Font defaultFont = new Font("Arial Black", Font.PLAIN, 20);
 
@@ -16,10 +20,15 @@ public class Program {
         for (int i = 0; i < 15; i++) {
             buttons.add(new JButton());
             buttons.get(i).setText(String.valueOf(i+1));
+            buttons.get(i).setSize(width,height);
+            buttons.get(i).setFont(defaultFont);
             panel.add(buttons.get(i));
         }
+        tommaRutan.setVisible(false);
+        panel.add(tommaRutan);
 
         panel.setLayout(new GridLayout(4,4));
+
 
 
 
