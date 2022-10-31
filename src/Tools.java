9cbab3318f0299aa6a7fc80dd781bc7c5a3a4475
Stefan <stefra90@live.se>
2022-10-31 12:16:06 +0100

    Check if puzzle is solvable

--- src/Tools.java
+++ src/Tools.java
@@ -39,7 +39,41 @@ public class Tools {
         }
         buttons.get(15).setVisible(false);
 }
+    public boolean isSolvable(ArrayList<Integer> randomizer)
+    {
+        int parity = 0;
+        int gridWidth = (int) Math.sqrt(randomizer.size());
+        int row = 0; // the current row we are on
+        int blankRow = 0; // the row with the blank tile
 
+        for (int i = 0; i < randomizer.size(); i++)
+        {
+            if (i % gridWidth == 0) { // advance to next row
+                row++;
+            }
+            if (randomizer.get(i) == 0) { // the blank tile
+                blankRow = row; // save the row on which encountered
+                continue;
+            }
+            for (int j = i + 1; j < randomizer.size(); j++)
+            {
+                if (randomizer.get(i) > randomizer.get(j) && randomizer.get(j) != 0)
+                {
+                    parity++;
+                }
+            }
+        }
+
+        if (gridWidth % 2 == 0) { // even grid
+            if (blankRow % 2 == 0) { // blank on odd row; counting from bottom
+                return parity % 2 == 0;
+            } else { // blank on even row; counting from bottom
+                return parity % 2 != 0;
+            }
+        } else { // odd grid
+            return parity % 2 == 0;
+        }
+    }
     public void checkIfMove(ArrayList<JButton> buttons,int i, JFrame frame){
 
         int originalX = buttons.get(i).getX();
